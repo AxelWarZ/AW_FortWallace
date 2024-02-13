99976dd678d6ee4c8666330f7f4e49b298eb213c
Alessio <98468547+AxelWarZ@users.noreply.github.com>
2024-02-13 23:00:13 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+author 'AxelWarZ'
+
+fx_version "adamant"
+game "rdr3"
+
+version '1.0.1'
+
+this_is_a_map 'yes'
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+server_scripts {
+    'server.lua',
+}
